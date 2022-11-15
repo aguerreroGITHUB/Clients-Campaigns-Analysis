19288aae5d202756ee8daef08c6c2ffb45b53b3d
Antonio Guerrero <117732324+aguerreroGITHUB@users.noreply.github.com>
2022-11-15 18:38:18 +0100

    Update CampaignsPerformance.sql

--- CampaignsPerformance.sql
+++ CampaignsPerformance.sql
@@ -1,1 +1,53 @@
-
+--- Create the averages table as a view ---
+CREATE VIEW [campaign_means] AS
+    Select count(ID)as n, avg(MntWines) as wines, avg(MntFruits) as fruits, avg(MntMeatProducts) as meat, 
+    avg(MntFishProducts) as fish, avg(MntSweetProducts) as sweets, avg(MntGoldProds) as gold
+    from companyData
+    where AcceptedCmp1 = 1 
+    UNION ALL
+    Select count(ID)as n, avg(MntWines) as wines, avg(MntFruits) as fruits, avg(MntMeatProducts) as meat, 
+    avg(MntFishProducts) as fish, avg(MntSweetProducts) as sweets, avg(MntGoldProds) as gold
+    from companyData
+    where AcceptedCmp2 = 1 
+    UNION ALL
+    Select count(ID)as n, avg(MntWines) as wines, avg(MntFruits) as fruits, avg(MntMeatProducts) as meat, 
+    avg(MntFishProducts) as fish, avg(MntSweetProducts) as sweets, avg(MntGoldProds) as gold
+    from companyData
+    where AcceptedCmp3 = 1 
+    UNION ALL
+    Select count(ID)as n, avg(MntWines) as wines, avg(MntFruits) as fruits, avg(MntMeatProducts) as meat, 
+    avg(MntFishProducts) as fish, avg(MntSweetProducts) as sweets, avg(MntGoldProds) as gold
+    from companyData
+    where AcceptedCmp4 = 1 
+    UNION ALL
+    Select count(ID)as n, avg(MntWines) as wines, avg(MntFruits) as fruits, avg(MntMeatProducts) as meat, 
+    avg(MntFishProducts) as fish, avg(MntSweetProducts) as sweets, avg(MntGoldProds) as gold
+    from companyData
+    where AcceptedCmp5 = 1 ;
+    
+--- Create the stdevs tabke as a view ---
+CREATE VIEW [campaign_stdevs] AS 
+    Select count(ID)as n, stdev(MntWines) as wines, stdev(MntFruits) as fruits, stdev(MntMeatProducts) as meat, 
+    stdev(MntFishProducts) as fish, stdev(MntSweetProducts) as sweets, stdev(MntGoldProds) as gold
+    from companyData 
+    where AcceptedCmp1 = 1 
+    UNION ALL
+    Select count(ID)as n, stdev(MntWines) as wines, stdev(MntFruits) as fruits, stdev(MntMeatProducts) as meat, 
+    stdev(MntFishProducts) as fish, stdev(MntSweetProducts) as sweets, stdev(MntGoldProds) as gold
+    from companyData 
+    where AcceptedCmp2 = 1 
+    UNION ALL
+    Select count(ID)as n, stdev(MntWines) as wines, stdev(MntFruits) as fruits, stdev(MntMeatProducts) as meat, 
+    stdev(MntFishProducts) as fish, stdev(MntSweetProducts) as sweets, stdev(MntGoldProds) as gold
+    from companyData 
+    where AcceptedCmp3 = 1 
+    UNION ALL
+    Select count(ID)as n, stdev(MntWines) as wines, stdev(MntFruits) as fruits, stdev(MntMeatProducts) as meat, 
+    stdev(MntFishProducts) as fish, stdev(MntSweetProducts) as sweets, stdev(MntGoldProds) as gold
+    from companyData 
+    where AcceptedCmp4 = 1 
+    UNION ALL
+    Select count(ID)as n, stdev(MntWines) as wines, stdev(MntFruits) as fruits, stdev(MntMeatProducts) as meat, 
+    stdev(MntFishProducts) as fish, stdev(MntSweetProducts) as sweets, stdev(MntGoldProds) as gold
+    from companyData 
+    where AcceptedCmp5 = 1 ;
