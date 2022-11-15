37c0019727e9530ff4cfdab0e586fcfcce2e746c
Antonio Guerrero <117732324+aguerreroGITHUB@users.noreply.github.com>
2022-11-15 12:17:54 +0100

    Update DataCleaningChunks.sql

--- DataCleaningChunks.sql
+++ DataCleaningChunks.sql
@@ -1,1 +1,6 @@
-
+--- LOOKING FOR DUPLICATED IDs ---
+SELECT 
+  count(ID) as IDs, -- how many IDs
+	count(distinct(ID)) as distIDs, -- how many distinct IDs. Should be the same as IDs if there are no duplicateds.
+	(select count(ID) from dbo.companyData where ID is null) as nulls -- subquery that counts nulls
+FROM dbo.companyData
