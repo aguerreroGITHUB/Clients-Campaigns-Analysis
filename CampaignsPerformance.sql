ca551076ff7bb44c3f48f7ec2c99495ed90b9c9a
Antonio Guerrero <117732324+aguerreroGITHUB@users.noreply.github.com>
2022-11-15 18:46:21 +0100

    Update CampaignsPerformance.sql

--- CampaignsPerformance.sql
+++ CampaignsPerformance.sql
@@ -1,3 +1,14 @@
+--- Create the global average sales stats table as a view ---
+CREATE VIEW [global_sales] AS 
+	select count(ID)as n, 
+		avg(MntWines) as wines, 
+		avg(MntFruits) as fruits, 
+		avg(MntMeatProducts) as meat, 
+		avg(MntFishProducts) as fish, 
+		avg(MntSweetProducts) as sweets, 
+		avg(MntGoldProds) as gold
+	from companyData
+    ;
 --- Create the averages table as a view ---
 CREATE VIEW [campaign_means] AS
     Select count(ID)as n, avg(MntWines) as wines, avg(MntFruits) as fruits, avg(MntMeatProducts) as meat, 
@@ -23,7 +34,8 @@ CREATE VIEW [campaign_means] AS
     Select count(ID)as n, avg(MntWines) as wines, avg(MntFruits) as fruits, avg(MntMeatProducts) as meat, 
     avg(MntFishProducts) as fish, avg(MntSweetProducts) as sweets, avg(MntGoldProds) as gold
     from companyData
-    where AcceptedCmp5 = 1 ;
+    where AcceptedCmp5 = 1 
+    ;
     
 --- Create the stdevs tabke as a view ---
 CREATE VIEW [campaign_stdevs] AS 
@@ -50,4 +62,5 @@ CREATE VIEW [campaign_stdevs] AS
     Select count(ID)as n, stdev(MntWines) as wines, stdev(MntFruits) as fruits, stdev(MntMeatProducts) as meat, 
     stdev(MntFishProducts) as fish, stdev(MntSweetProducts) as sweets, stdev(MntGoldProds) as gold
     from companyData 
-    where AcceptedCmp5 = 1 ;
+    where AcceptedCmp5 = 1 
+    ;
