af326bf9e9561cd02f37cd82c118846a597f9afb
Antonio Guerrero <117732324+aguerreroGITHUB@users.noreply.github.com>
2022-11-16 11:58:45 +0100

    Update SellingPlatformsChunks.sql

--- SellingPlatformsChunks.sql
+++ SellingPlatformsChunks.sql
@@ -1,1 +1,11 @@
+--- Catalog vs Stores comparison ---
+Select sum(NumCatalogPurchases) as Catalog, 
+	sum(NumStorePurchases) as Stores, 
+	100*cast(sum(NumCatalogPurchases) as float)/cast(sum(NumStorePurchases)as float) as ProportionPerc
+from companyData;
 
+--- Web vs Stores comparison ---
+Select sum(NumWebPurchases) as Web, 
+	sum(NumStorePurchases) as Stores, 
+	100*cast(sum(NumWebPurchases) as float)/cast(sum(NumStorePurchases)as float) as ProportionPerc
+from companyData;
